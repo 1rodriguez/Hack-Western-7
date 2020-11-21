ba46b66cd297a3b0fb6ad2282456bd3d13c4c0f5
Hans Krohn <hanskrohn70@gmail.com>
2020-11-21 03:23:10 -0500

    connecting to python file, libraries not working

--- backend/api/python/main.py
+++ backend/api/python/main.py
@@ -1,18 +1,23 @@
 import speech_recognition as sr
 import moviepy.editor as mp
+import sys 
+
 
 # get video and convert to audtio
-def convertToTxt():
-    # videoName = mp4
-    # clip = mp.VideoFileClip(videoName)
-    # clip.audio.write_audiofile(r"converted.wav")
-    # r = sr.Recognizer()
-    # audio = sr.AudioFile("converted.wav")
+videoName = "sampleVideo.mp4"
+clip = mp.VideoFileClip(videoName)
+clip.audio.write_audiofile(r"converted.wav")
+r = sr.Recognizer()
+audio = sr.AudioFile("converted.wav")
 
-    # # read in speech to text file
-    # with audio as source:
-    #     audio_file = r.record(source)
+# read in speech to text file
+with audio as source:
+    audio_file = r.record(source)
 
-    # result = r.recognize_google(audio_file)
+result = r.recognize_google(audio_file)
 
-    return "hi"
+
+
+
+
+
