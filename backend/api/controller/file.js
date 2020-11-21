8d68169663702c3c9b88f175b8f234e7047a6a19
Hans Krohn <hanskrohn70@gmail.com>
2020-11-21 04:02:16 -0500

    corrected files

--- backend/api/controller/file.js
+++ backend/api/controller/file.js
@@ -1,23 +1,20 @@
 exports.convert = async (req, res) => {
     try{
         const {file} = req.body;
+        var spawn = require("child_process").spawn; 
+        var process = spawn('python',["./api/python/main.py", file] );
 
-        const spawn = require("child_process").spawn;
-        const pp = spawn('python',["../python/venv/main.py"]);
+        process.stdout.on('data', (data) => console.log(data.toString()));
 
-        pp.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-        });
-
-        pp.stderr.on('data', (data) => {
+        process.stderr.on('data', (data) => {
         console.log(`stderr: ${data}`);
         });
 
-        pp.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-        });
+        // pp.on('close', (code) => {
+        // console.log(`child process exited with code ${code}`);
+        // });
 
-        console.log(pp)
+        // console.log(pp)
         // const txt = pythonProcess.convertToTxt(file);
 
         return res.status(200).send({
