c67bff04f90ebb5dde9e38eddee4e427b3dc8167
Kwame Bonsu <kwamebonsu2010@gmail.com>
2020-11-21 18:29:15 -0500

    removed empty file

--- backend/api/python/.idea/vidSpeed.py
+++
@@ -1,8 +0,0 @@
-from moviepy.editor import *
-import sys
-# get speed as argument
-speed = int(sys.argv[0])
-clip = VideoFileClip("sampleVideo.mp4")
-# change speed
-final = clip.fx(vfx.speedx, speed)
-final.write_videofile("edited.mp4")
