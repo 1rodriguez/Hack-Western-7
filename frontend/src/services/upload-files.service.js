d48e4adef573b289443b14b090c50b75a99cc0f6
Hans Krohn <hanskrohn70@gmail.com>
2020-11-22 01:57:26 -0500

    download .txt files

--- frontend/src/services/upload-files.service.js
+++ frontend/src/services/upload-files.service.js
@@ -1,15 +1,27 @@
 import http from '../http-common';
+import download from 'downloadjs';
 
 class UploadFilesService {
     upload(file) {
         const formData = new FormData();
 
-        formData.append("file", file);
-        console.log(file);
-
-        http.post('/convert', formData).then( res => {
-            console.log(res.statusText);
+        formData.append("video", file);
+        
+        fetch('http://localhost:5000/api/convert',{
+        method: 'POST',
+        credentials: 'include',
+        body: formData
         })
+      .then(res => {
+          console.log(res)
+         
+          return res.blob()
+        })
+      .then(body => {
+          console.log(body)
+          download(body, 'something.txt', 'text/plain')
+        })
+     
     }
 }
 
